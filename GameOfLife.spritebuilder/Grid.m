54f23df21c6c75cc465bb9f8312b190464443511
Jigar Patel <jigar@Jigars-MBP.sv.cmu.local>
2015-01-20 12:06:29 -0800

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -79,5 +79,8 @@ static const int GRID_COLUMNS = 10;
 - (Creature *)creatureForTouchPosition:(CGPoint)touchPosition
 {
     //get the row and column that was touched, return the Creature inside the corresponding cell
+    int row = touchPosition.y / _cellHeight;
+    int col = touchPosition.x / _cellWidth;
+    return _gridArray[row][col];
 }
 @end
